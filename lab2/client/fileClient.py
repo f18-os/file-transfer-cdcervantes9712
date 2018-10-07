6e32a82d9c64b2d3ad720b424613eeb6446958b0
Carlos Cervantes <31900188+cdcervantes9712@users.noreply.github.com>
2018-10-07 10:20:27 -0600

    Add files via upload
    This client program can send (PUT) files to the server. However, in the current state, only one client can be handled at a time. Multiple client functionality needs to be implemented.

---
+++ lab2/client/fileClient.py
@@ -0,0 +1,41 @@
+from socket import *
+import sys
+
+def sendFileToServer(clientSocket, file_name):
+    try:
+        with open(file_name, "r") as clientFile:
+            print ("Sending file...")
+            data = clientFile.read()
+            clientSocket.send(data.encode('utf-8'))
+            clientFile.close()
+    except IOError as e:
+        print ("No such file or directory. Try again.")
+        clientSocket.send("File not found. Try again.")
+        return
+    print ("Successfuly sent file to server.")
+    return
+
+def connectSocket():
+    clientSocket = socket()                 #create a socket object
+    clientAddr = ("127.0.0.1", 50000)       #set address and reserve port
+    try:
+        clientSocket.connect(clientAddr)
+        print ("Connected to server.")
+    except:
+        print ("Error. Server not found.")
+        sys.exit()
+    return clientSocket
+
+def startClient():
+    try:
+        file_name = sys.argv[1]                 #get file name from argument
+    except:
+        print ("Wrong parameters.")
+        sys.exit()
+    clientSocket = connectSocket()
+    clientSocket.send((file_name + ":PUT").encode('utf-8'))     #send file name and choice to server
+    sendFileToServer(clientSocket, file_name.encode('utf-8'))
+    clientSocket.close()
+    print ("Connection closed.")
+
+startClient()
